677144062c211591f4ad94ed67b7c7d65b726d43
Geetinder <dhillon.geetu@gmail.com>
2021-06-01 14:59:03 +0530

    second commit

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -2,7 +2,7 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//testNG run
 @CucumberOptions(
 		features= {"src\\test\\resources\\features"},
 		glue= {"steps","hooks"},
